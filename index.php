ad646aefa7f798cff8452cb7831c7e86d9313740
Saurav235884 <107362365+Saurav235884@users.noreply.github.com>
2022-06-19 13:21:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,385 @@
+<?php include_once('header.php'); ?>
+
+    <!-- Header Section End -->
+    
+    <!-- Home Section Start -->
+    <section class="home" id="home">
+        <div class="image">
+            <!-- <img src="images/BkApp.jpg" alt=""> -->
+        </div>
+        <div class="content">
+            <h3>stay Safe, Stay Healthy</h3>
+            <p>Stay home stay safe. Follow the Golden Rule. Wash your hands frequently with soap and water or use alchohol based Sanitizer and Wear Mask.</p>
+            <a href="#" class="btn">Take Appoinment <span class="fas fa-chevron-right"></span></a>
+        </div>
+    </section>
+    <!-- Home Section End -->
+    
+    <!-- icon section start  -->
+    <section class="icon-container">
+        <div class="icons">
+            <i class="fas fa-user-md"></i>
+            <h3 class="counter" data-target="140+">0</h3>
+            <p>Doctors at work</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-users"></i>
+            <h3 class="counter" data-target="2033">0</h3>
+            <p>Satisfied Patients</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-users"></i>
+            <h3 class="counter" data-target="500">0</h3>
+            <p>Bed Facility</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-hospital"></i>
+            <h3 class="counter" data-target="80<">0</h3>
+            <p>available hospitals</p>
+        </div>
+    </section>
+    <!-- icon section end -->
+
+    <!-- About Section Start  -->
+    <section class="about" id="about">
+
+        <h1 class="heading"><span>About</span> Us</h1>
+        <div class="row">
+            <div class="image">
+                <img src="./images/india-doctor.jpg" alt="">
+            </div>
+        
+            <div class="content">
+                <h3>we take care of your healthy life</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, quam voluptas, omnis nesciunt molestias facere repudiandae id nemo doloremque est, minus nisi quisquam ratione ducimus maxime corporis consectetur beatae illo.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non, provident perferendis quam, iure inventore unde sint tempora consequuntur consectetur.</p>
+                <a href="#" class="btn"> Read more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+    </section>
+    <!-- About Section End  -->
+
+    <!-- Service Section Start -->
+    <section class="services" id="services">
+
+        <h1 class="heading">Our <span>Services</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <i class="fas fa-notes-medical"></i>
+                <h3>Free Checkup</h3>
+                <p>If any member of the household shows symptoms of Covid-19, seek medical advice and follow your local health authority's guidance.</p>
+                <a href="#" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-ambulance"></i>
+                <h3>24/7 Ambulance</h3>
+                <p>To request an ambulance for a COVID-19-positive individual, contact Ambulance Worldwide today.Our Ambulance coordinators are standing by 24/7 to assist you.</p>
+                <a href="#" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-user-md"></i>
+                <h3>Expert Doctors</h3>
+                <p>Doctors who sacrificed their life while saving patients during the ongoing COVID-19 pandemic.</p>
+                <a href="#" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-pills"></i>
+                <h3>Medicines</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptatem nostrum temporibus odio, expedita sunt porro vitae.</p>
+                <a href="#" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-heartbeat"></i>
+                <h3>Proper Care</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptatem nostrum temporibus odio, expedita sunt porro vitae.</p>
+                <a href="#" class="btn">Read More <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+        </div>
+    </section>
+    <!-- Service Section End -->
+
+    <!-- Doctor Section Start -->
+    <section class="doctors" id="doctors">
+        <h1 class="heading">Our <span>Doctors</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="./images/Dr3.jpg" alt="">
+                <h3>Dr. Sakshi Ghildiyal</h3>
+                <span>Expert Doctor</span>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-telegram"></a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="./images/Dr2.jpg" alt="">
+                <h3>Dr. Laxmi Priya</h3>
+                <span>Expert Doctor</span>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-telegram"></a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="./images/Dr6.jpg" alt="">
+                <h3>Dr. Rajeev Chand</h3>
+                <span>Expert Doctor</span>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-telegram"></a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="./images/Dr4.jpg" alt="">
+                <h3>Dr. Maurya Singh</h3>
+                <span>Expert Doctor</span>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-telegram"></a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./images/Dr5.jpg" alt="">
+                <h3>Dr. Aakash</h3>
+                <span>Expert Doctor</span>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-telegram"></a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="./images/riyadr(1).jpg" alt="">
+                <h3>Dr. Riya Singhal</h3>
+                <span>Expert Doctor</span>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-telegram"></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Doctor Section end -->
+
+    <!-- Booking Section Start -->
+    <section class="book" id="book">
+        <h1 class="heading"><span>Book</span> Appoinment</h1>
+        <div class="row">
+            <div class="image">
+                <img src="./images/BkApp.jpg" alt="">
+            </div>
+            <form action="">
+                <h3>Take Appoinment</h3>
+                <input type="text" placeholder="your name" class="box">
+                <input type="text" placeholder="your number" class="box">
+                <input type="email" placeholder="your email" class="box">
+                <input type="date"  class="box">
+                <input type="submit" value="book now" class = "btn">
+            </form>
+        </div>
+    </section>
+    <!-- Booking Section End -->
+
+    <!-- Review Section Start -->
+    <section class="review" id="review">
+        <h1 class="heading">Client's <span>Reviews</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="./images/client1.jpg" alt="">
+                <h3>Ankit Sharma</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eveniet, voluptatem unde laboriosam ad debitis perferendis facere architecto pariatur earum, atque in minus. Corporis earum quam nulla nobis magnam ut?</p>
+            </div>
+
+            <div class="box">
+                <img src="./images/client2.jpg" alt="">
+                <h3>Chandra Mohan</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eveniet, voluptatem unde laboriosam ad debitis perferendis facere architecto pariatur earum, atque in minus. Corporis earum quam nulla nobis magnam ut?</p>
+            </div>
+
+            <div class="box">
+                <img src="./images/client3.jpg" alt="">
+                <h3>Prince Kumar</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eveniet, voluptatem unde laboriosam ad debitis perferendis facere architecto pariatur earum, atque in minus. Corporis earum quam nulla nobis magnam ut?</p>
+            </div>
+
+            <div class="box">
+                <img src="./images/client4.1.jpg" alt="">
+                <h3>Aakash sharma</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eveniet, voluptatem unde laboriosam ad debitis perferendis facere architecto pariatur earum, atque in minus. Corporis earum quam nulla nobis magnam ut?</p>
+            </div>
+        </div>
+    </section>
+    <!-- Review Section End -->
+
+    <!-- Blogs section Start -->
+    <section class="blogs" id="blogs">
+
+        <h1 class="heading">Our <span>Blogs</span></h1>
+
+        <div class="box-container">
+            <div class="box">
+                <div class="image">
+                    <img src="./images/checkcovid.jpg" alt="">
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <a href="#"><i class="fas fa-calendar"></i> 8th mar, 2022</a>
+                        <a href="#"><i class="fas fa-user"></i> by admin</a>
+                    </div>
+                    <h3>Blog Title Goes Here</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus atque in repellat, quisquam commodi! Aut ipsa culpa dolores error sint.</p>
+                    <a href="#" class="btn">Read more <span class="fas fa-chevron-right"></span></a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="./images/checkcovid1.jpg" alt="">
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <a href="#"><i class="fas fa-calendar"></i> 8th mar, 2022</a>
+                        <a href="#"><i class="fas fa-user"></i> by admin</a>
+                    </div>
+                    <h3>Blog Title Goes Here</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus atque in repellat, quisquam commodi! Aut ipsa culpa dolores error sint.</p>
+                    <a href="#" class="btn">Read more <span class="fas fa-chevron-right"></span></a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="./images/vacine.jpg" alt="">
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <a href="#"><i class="fas fa-calendar"></i> 8th mar, 2022</a>
+                        <a href="#"><i class="fas fa-user"></i> by admin</a>
+                    </div>
+                    <h3>Blog Title Goes Here</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus atque in repellat, quisquam commodi! Aut ipsa culpa dolores error sint.</p>
+                    <a href="#" class="btn">Read more <span class="fas fa-chevron-right"></span></a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- Blogs section End -->
+
+    <!-- Footer Section Start -->
+
+    <section class="footer">
+        <div class="box-container">
+            <div class=" icons">
+                <h3><i class="fas fa-heartbeat"></i> Medino</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam atque dicta ducimus quibusdam porro quidem aspernatur vero iste blanditiis nihil velit minus cum consequatur, impedit molestias voluptatum, ad accusamus! Dolores?</p>
+                <a href="#"><i class="fab fa-facebook-square"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+            </div>
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="#"><i class="fas fa-chevron-right"></i> Home</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> About</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Services</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Doctors</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Book</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Review</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Blogs</a>
+            </div>
+            <div class="box">
+                <h3>Our Services</h3>
+                <a href="#"><i class="fas fa-chevron-right"></i> Covid Checkup</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> 24/7 Ambulance</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Therapy</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Doctors Checkup</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Diagnosis</a>
+                <a href="#"><i class="fas fa-chevron-right"></i> Dental Care</a> 
+            </div>
+            <div class="box">
+                <h3>Our Services</h3>
+                <a href="#" ><i class="fas fa-phone"></i> +119-5553-5553</a>
+                <a href="#" ><i class="fas fa-phone"></i> +119-5553-5554</a>
+                <a href="#" ><i class="fas fa-envelope"></i> Medino@gmail.com</a>
+                <a href="#" ><i class="fas fa-envelope"></i> MedinoEmr@gmail.com</a>
+                <!-- <a href="#" ><i class="fas fa-map-marker-alt"></i> Delhi, India - 110044</a> -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28046.256384370274!2d77.31330405000001!3d28.51620445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce65f77cae949%3A0x7c768dbb3f78e57!2sBadarpur%2C%20New%20Delhi%2C%20Delhi%2C%20India!5e0!3m2!1sen!2sde!4v1654709490380!5m2!1sen!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            
+        </div>
+
+        <div class="credit"> Create By <span>Saurav Kumar</span> | All Right Reserved</div>
+    </section>
+
+    <!-- Footer Section End -->
+
+
+
+
+
+
+<!-- Script js -->
+
+<!-- Swiper JS -->
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<!-- Swiper JS -->
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+
+<script src="./script/main.js"></script>
+<!-- Script js -->
+
+
+</body>
+</html>
